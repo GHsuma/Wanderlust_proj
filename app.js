3fb5e4a1b8948e9bd8fb8cddb4b74a79f9f9dcab
suma_borannawar#26 <139582616+GHsuma@users.noreply.github.com>
2025-03-31 23:24:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV!="production"){
     require('dotenv').config()
 
 }
-console.log(process.env.SECRET)
+
 
 
 const express=require("express")
@@ -127,9 +127,7 @@ console.log("sample saved");
 res.send("Successfully saved")
 })
 
-app.get("/sample",async(req,res)=>{
-    
-})
+
 
 
  
